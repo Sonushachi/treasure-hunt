3b10a75465d09b053d159ab0c1d9ec92e06b2d5d
Sonushachi <73229857+Sonushachi@users.noreply.github.com>
2021-07-20 22:26:03 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+
+    fill("white");
+    textSize(15);
+    text("A T A D A S B E", 700,150);
+    fill("lightblue");
+    text("Hint: Organized collection of data !!", 700,170);
+
+    fill("white");
+    textSize(15);
+    text("C U T N I F O N", 100,350);
+    fill("lightblue");
+    text("Hint: An activity that is natural to or the purpose of a person or thing. !!", 100,370);
+
+    
+    
+    
+
+}
